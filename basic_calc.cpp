edf4ebb429ba41f1388ec07769ce4a5271280c84
tamir avraham <tamir.avrah@gmail.com>
2022-12-15 22:53:51 +0200

    did basic calc

---
+++ basic_calc.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+/// <summary>
+///  error handeling 
+/// </summary>
+/// <param name="resultOfMath">int to see if is error number </param>
+//void CheackIfError(int resultOfMath) {
+//    if (resultOfMath==8200)
+//    {
+//        std::cerr << "This user is not authorized to access 8200, please enter different numbers, or try to get clearance in 1 year\n";
+//    }
+//}
+//
+//int add(int a, int b) {
+//    int res = a + b;
+//    CheackIfError(res);
+//    return res;
+//}
+//
+//int  multiply(int a, int b) {
+//    int sum = 0;
+//    for (int i = 0; i < b; i++) {
+//        sum = add(sum, a);
+//    };
+//    CheackIfError(sum);
+//    return sum;
+//}
+//
+//int  pow(int a, int b) {
+//    int exponent = 1;
+//    for (int i = 0; i < b; i++) {
+//        exponent = multiply(exponent, a);
+//    };
+//    CheackIfError(exponent);
+//    return exponent;
+//}
+
+//int main(void) {
+//    std::cout << pow(5, 5) << std::endl;
+//}
